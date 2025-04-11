fa8a07589653519337d29966a7826e54da700285
Unheat <anbi2682006@gmail.com>
2025-04-11 00:28:50 -0400

    final

--- script.js
+++ script.js
@@ -3,44 +3,63 @@ function getRandomIntInclusive(min, max) {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
   }
-var choice = prompt("what is your choice (rock, sccisor, papaer):");
-var computer_choice = getRandomIntInclusive(1,3);
-switch (computer_choice) {
-    case 1:
-        computer_choice = "paper";
-        break;
-    case 2:
-        computer_choice = "rock";
-        break;
-    case 3:
-        computer_choice = "sccisor";
-        break;
-  }
-  if(choice == "paper"){
-    if(computer_choice == "paper"){
-        console.log("draw");
+function play_game(){
     
-    } else if(computer_choice == "rock"){
-        console.log("win");
-    } else {
-        console.log("lose");
-    }
-  } else if(choice == "rock"){
-    if(computer_choice == "paper"){
-        console.log("lose");
-    
-    } else if(computer_choice == "rock"){
-        console.log("draw");
-    } else {
-        console.log("win");
-    }
- } else {
-    if(computer_choice == "paper"){
-        console.log("win");
-    
-    } else if(computer_choice == "rock"){
-        console.log("lose");
-    } else {
-        console.log("draw");
-    }
-    }
+    const button  = document.querySelectorAll(".button");
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    button.forEach(button => {
+        if(!button.classList.contains("listener-attached")){
+            button.addEventListener("click", function (e) {
+                let choice = e.target.classList[1];
+                let computer_choice = getRandomIntInclusive(1,3);
+                
+                switch (computer_choice) {
+                    case 1:
+                        computer_choice = "paper";
+                        break;
+                    case 2:
+                        computer_choice = "rock";
+                        break;
+                    case 3:
+                        computer_choice = "scissor";
+                        break;
+                }
+                if(choice == "paper"){
+                    if(computer_choice == "paper"){
+                        div.innerText ="draw";
+                    
+                    } else if(computer_choice == "rock"){
+                        div.innerText ="win";
+                    } else {
+                        div.innerText ="lose";
+                    }
+                } else if(choice == "rock"){
+                    if(computer_choice == "paper"){
+                        div.innerText ="lose";
+                    
+                    } else if(computer_choice == "rock"){
+                        div.innerText ="draw";
+                    } else {
+                        div.innerText ="win";
+                    }
+                } else {
+                    if(computer_choice == "paper"){
+                        div.innerText ="win";
+                    
+                    } else if(computer_choice == "rock"){
+                        div.innerText ="lose";
+                    } else {
+                        div.innerText ="draw";
+                    }
+                    }
+                    
+                    return;
+                });
+                button.classList.add("listener-attached");
+            }
+        });
+}
+const play = document.querySelector(".play");
+play.addEventListener("click", play_game);
+
